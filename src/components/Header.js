fdfd1e4378134eb2ce2cc4be391b85666ae683da
Ritwik <shawritwik2@gmail.com>
2022-07-15 11:19:04 +0530

    Complete User Authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { connect } from "react-redux";
+import { signOutAPI } from "../actions";
 
 const Header = (props) => {
   return (
@@ -52,17 +54,26 @@ const Header = (props) => {
 
             <User>
               <a>
-                <img src="/images/user.svg" alt="" />
+                {props.user && props.user.photoURL ? (
+                  <img
+                    src={props.user.photoURL}
+                    alt=""
+                    referrerpolicy="no-referrer"
+                  />
+                ) : (
+                  <img src="/images/user.svg" alt="" />
+                )}
                 <span>
                   Me
                   <img src="/images/down-icon.svg" alt="" />
                 </span>
               </a>
 
-              <SignOut>
+              <SignOut onClick={() => props.signOut()}>
                 <a>Sign Out</a>
               </SignOut>
             </User>
+
             <Work>
               <a>
                 <img src="/images/nav-work.svg" alt="" />
@@ -262,4 +273,14 @@ const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  signOut: () => dispatch(signOutAPI()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
